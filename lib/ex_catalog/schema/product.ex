0945af97f06eb11ecfb675c7a494ce39f4e1dfe8
mithereal <mithereal@gmail.com>
2023-02-14 23:32:47 -0700

    .

--- lib/ex_catalog/schema/product.ex
+++ lib/ex_catalog/schema/product.ex
@@ -2,12 +2,6 @@ defmodule ExCatalog.Product do
   defstruct id: nil,
             sku: nil,
             price: 0,
-            qty: 0,
             name: nil,
-            description: nil,
-            type: nil,
-            length: nil,
-            height: nil,
-            width: nil,
-            weight: 0
+            description: nil
 end
