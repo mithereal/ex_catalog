d9c78a14558ff5fb5458af0b54cb400de4fb4395
mithereal <mithereal@gmail.com>
2023-11-17 03:42:41 -0700

    fix test

--- test/ex_catalog_test.exs
+++ test/ex_catalog_test.exs
@@ -41,19 +41,4 @@ defmodule ExCatalogTest do
     assert ExCatalog.product("12345", :USD).sku == "12345"
   end
 
-  test "test product" do
-    price = Money.new(:USD, 100)
-
-    product = %{
-      sku: "12345",
-      price: price,
-      title: "test product",
-      sub_title: "test product",
-      description: "test product"
-    }
-
-    ExCatalog.Product.new(product)
-
-    assert ExCatalog.product("12345", :USD).sku == "12345"
-  end
 end
